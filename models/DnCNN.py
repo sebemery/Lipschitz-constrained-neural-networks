7110c6e90ba19f85c3995be87a4394efbda6f7b3
Sébastien Emery <sebastien.emery@epfl.ch>
2021-03-08 14:03:31 +0100

    fix model for spectral norm

--- models/DnCNN.py
+++ models/DnCNN.py
@@ -1,23 +1,32 @@
 import torch
 import torch.nn.functional as F
+import utils
 from torch import nn
 
 
 class DnCNN(nn.Module):
-    def __init__(self, depth=7, n_channels=64, image_channels=1, kernel_size=3, padding=1, architecture="residual", spectral_norm =True):
+    def __init__(self, depth=7, n_channels=64, image_channels=1, kernel_size=3, padding=1, architecture="residual", spectral_norm ="Chen"):
         super(DnCNN, self).__init__()
         layers = []
         self.architecture = architecture
 
-        if spectral_norm:
-            layers.append(nn.utils.spectral_norm(nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
+        if spectral_norm == "Chen":
+            layers.append(utils.Spectral_Normalize_chen.spectral_norm(nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
             layers.append(nn.ReLU(inplace=True))
             for _ in range(depth - 2):
-                layers.append(nn.utils.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
+                layers.append(utils.Spectral_Normalize_chen.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
                 layers.append(nn.ReLU(inplace=True))
-            layers.append(nn.utils.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=image_channels, kernel_size=kernel_size, padding=padding)))
+            layers.append(utils.Spectral_Normalize_chen.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=image_channels, kernel_size=kernel_size, padding=padding)))
             self.dncnn = nn.Sequential(*layers)
-        else:
+        elif spectral_norm == "Normal":
+            layers.append(utils.Spectral_Normalize.spectral_norm(nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
+            layers.append(nn.ReLU(inplace=True))
+            for _ in range(depth - 2):
+                layers.append(utils.Spectral_Normalize.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding)))
+                layers.append(nn.ReLU(inplace=True))
+            layers.append(utils.Spectral_Normalize.spectral_norm(nn.Conv2d(in_channels=n_channels, out_channels=image_channels, kernel_size=kernel_size, padding=padding)))
+            self.dncnn = nn.Sequential(*layers)
+        elif spectral_norm == "None":
             layers.append(nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding))
             layers.append(nn.ReLU(inplace=True))
             for _ in range(depth - 2):
