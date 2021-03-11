e83e7461b33295ff85bccf8ed58c69cb2f92a957
Sébastien Emery <sebastien.emery@epfl.ch>
2021-03-11 11:04:54 +0100

    update slices

--- data/select_slices.py
+++ data/select_slices.py
@@ -90,6 +90,10 @@ class SelectSlices:
         print("The mean SNR is {}".format(SNR.mean()))
         print("The max SNR is {}".format(np.amax(SNR)))
         print("The min SNR is {}".format(np.amin(SNR)))
+        metrics = {"Mean SNR": SNR.mean(), "Max SNR": np.amax(SNR), "Min SNR": np.amin(SNR)}
+        with open('SNR.txt', 'w') as f:
+            for k, v in list(metrics.items()):
+                f.write("%s\n" % (k + ':' + f'{v}'))
 
 
 if __name__ == '__main__':
